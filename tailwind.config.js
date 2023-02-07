c39604450116186f008dc44b095615b2e2cd594f
Axel Enghamre <axel@enghamre.se>
2023-02-07 15:55:15 +0100

    fixed tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,20 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/*/.{js,ts,jsx,tsx}"],
-  theme: {
-    extend: {
-      extend: {
-        colors: {
-					atlantic: "#51A3A3",
-					mediterranean: "#414288",
-					pacific: "#83DBDB",
-					salt: "#F5F5F5",
-					baltic: "#E0777D",
-					abyss: "#000000",
-					clear: "#FFFFFF",
-        },
-      },
-    },
-  },
-  plugins: [],
+	content: ['./src/**/*.{js,ts,jsx,tsx}'],
+	theme: {
+		extend: {
+			colors: {
+				atlantic: '#51A3A3',
+				mediterranean: '#414288',
+				pacific: '#83DBDB',
+				salt: '#F5F5F5',
+				baltic: '#E0777D',
+				abyss: '#000000',
+				clear: '#FFFFFF',
+			},
+		},
+	},
+	plugins: [],
 };
