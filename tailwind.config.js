6ed6bd51d0cf85354bca2f5bad15704ed8b4a49d
Axel Enghamre <axel@enghamre.se>
2023-02-07 13:59:23 +0100

    Added tailwind colors and removed init code

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,17 @@
 module.exports = {
   content: ["./src/*/.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      extend: {
+        colors: {
+          lightCoral: "#e0777dff",
+          whiteSmoke: "#f5f5f5ff",
+          verdigris: "#51a3a3ff",
+          marianBlue: "#414288ff",
+          celestialBlue: "#1b98e0ff",
+        },
+      },
+    },
   },
   plugins: [],
 };
