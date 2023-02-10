493a4563b3a45368b152e78e262698f16de4e39d
Adishumla <45212964+Adishumla@users.noreply.github.com>
2023-02-10 14:29:27 +0100

    GB data fetch

--- src/components/data.tsx
+++ src/components/data.tsx
@@ -1,15 +1,9 @@
+import type { NextApiRequest, NextApiResponse } from 'next';
+import fetch from 'node-fetch';
 
-const url = "http://data.goteborg.se/RiverService/v1.1/Measurements&format=Json";
-
-function getData = async () =>{
-	const response = await fetch(url);
+const url = 'http://data.goteborg.se/RiverService/v1.1/MeasureSites/4a9a0d23-8e98-4e64-81ac-3e01fed82bee?format=Json';
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+	const response = await Promise.resolve(fetch(url));
 	const data = await response.json();
-	return (data);
-}
-const data = getData();
-
-export default function Data({}) {
-	return (
-		<p>{data}</p>
-	);
+	console.log(data);
 }
