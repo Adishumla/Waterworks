e2ab3aae6d461e5bd9a22d4cb9cfd9e0eb22493f
Adishumla <45212964+Adishumla@users.noreply.github.com>
2023-02-10 14:49:10 +0100

    fixed GB api

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
 import Hero from '@/components/Home/Hero';
+import Data from '@/components/data';
 
 export default function Home() {
 	return (
 		<>
 			<Hero />
+			<Data />
 			<main className='bg-gradient-to-b from-white to-atlantic min-h-screen'></main>
 		</>
-	)
+	);
 }
--- src/components/data.tsx
+++ src/components/data.tsx
@@ -1,9 +1,19 @@
 import type { NextApiRequest, NextApiResponse } from 'next';
-import fetch from 'node-fetch';
 
-const url = 'http://data.goteborg.se/RiverService/v1.1/MeasureSites/4a9a0d23-8e98-4e64-81ac-3e01fed82bee?format=Json';
-export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+export default async function displayData() {
+	const url = 'http://data.goteborg.se/RiverService/v1.1/MeasureSites/4a9a0d23-8e98-4e64-81ac-3e01fed82bee?format=Json';
 	const response = await Promise.resolve(fetch(url));
 	const data = await response.json();
-	console.log(data);
+	return (
+		<>
+			<h1>Display Data</h1>
+			{data.map((item: any) => (
+				<div key={item.id}>
+					<h2>{item.Code}</h2>
+					<p>{item.Description}</p>
+					<p>{item.Lat}</p>
+				</div>
+			))}
+		</>
+	);
 }
