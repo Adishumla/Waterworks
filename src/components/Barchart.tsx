0cd56164f64c6002f418eb5b9bf8707d8b984f66
Adishumla <45212964+Adishumla@users.noreply.github.com>
2023-02-10 16:10:19 +0100

    toke data

--- src/components/Barchart.tsx
+++ src/components/Barchart.tsx
@@ -1,38 +1,46 @@
 'use client';
 import Utils from 'chart.js';
 import { Bar } from 'react-chartjs-2';
-
 import { Chart, registerables } from 'chart.js';
+import ChartData from '@/components/ChartData';
+
+const car = ChartData();
+
+car.then((carData) => {
+	const chartData = carData.chartData;
+	const carLat = chartData.map((item: any) => item.Lat);
+	const data = {
+		labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+		datasets: [
+			{
+				label: '# of Votes',
+				data: [carLat],
+				backgroundColor: [
+					'rgba(255, 99, 132, 0.2)',
+					'rgba(54, 162, 235, 0.2)',
+					'rgba(255, 206, 86, 0.2)',
+					'rgba(75, 192, 192, 0.2)',
+					'rgba(153, 102, 255, 0.2)',
+					'rgba(255, 159, 64, 0.2)'
+				],
+				borderColor: [
+					'rgba(255, 99, 132, 1)',
+					'rgba(54, 162, 235, 1)',
+					'rgba(255, 206, 86, 1)',
+					'rgba(75, 192, 192, 1)',
+					'rgba(153, 102, 255, 1)',
+					'rgba(255, 159, 64, 1)'
+				],
+				borderWidth: 1
+			}
+		]
+	};
+	return data;
+});
 
 Chart.register(...registerables);
 
 //create a new chart with Bar from react-chartjs-2
-const data = {
-	labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
-	datasets: [
-		{
-			label: '# of Votes',
-			data: [12, 19, 3, 5, 2, 3],
-			backgroundColor: [
-				'rgba(255, 99, 132, 0.2)',
-				'rgba(54, 162, 235, 0.2)',
-				'rgba(255, 206, 86, 0.2)',
-				'rgba(75, 192, 192, 0.2)',
-				'rgba(153, 102, 255, 0.2)',
-				'rgba(255, 159, 64, 0.2)'
-			],
-			borderColor: [
-				'rgba(255, 99, 132, 1)',
-				'rgba(54, 162, 235, 1)',
-				'rgba(255, 206, 86, 1)',
-				'rgba(75, 192, 192, 1)',
-				'rgba(153, 102, 255, 1)',
-				'rgba(255, 159, 64, 1)'
-			],
-			borderWidth: 1
-		}
-	]
-};
 
 const options = {
 	scales: {
@@ -46,7 +54,7 @@ export default function Barchart() {
 	return (
 		<div>
 			<p>bar-chart</p>
-			<Bar data={data} options={options} />
+			<Bar data={car} options={options} />
 		</div>
 	);
 }
--- src/components/ChartData.tsx
+++ src/components/ChartData.tsx
@@ -4,8 +4,10 @@ export default async function displayData() {
 	const url = 'http://data.goteborg.se/RiverService/v1.1/MeasureSites/4a9a0d23-8e98-4e64-81ac-3e01fed82bee?format=Json';
 	const response = await Promise.resolve(fetch(url));
 	const chartData = await response.json();
-	return (
-		<>
+	return {
+		chartData
+
+		/* <>
 			<h1>Display Data</h1>
 			{chartData.map((item: any) => (
 				<div key={item.id}>
@@ -14,6 +16,6 @@ export default async function displayData() {
 					<p>{item.Lat}</p>
 				</div>
 			))}
-		</>
-	);
+		</> */
+	};
 }
