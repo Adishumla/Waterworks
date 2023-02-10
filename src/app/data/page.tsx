be8bb73850621f21e61284aa42da242eb15dbca1
Axel Enghamre <axel@enghamre.se>
2023-02-10 13:33:22 +0100

    moved barchat to data route

---
+++ src/app/data/page.tsx
@@ -0,0 +1,8 @@
+import Barchart from '@/components/Barchart';
+export default function Data() {
+	return (
+		<main>
+			<Barchart />
+		</main>
+	);
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,7 @@
-import Barchart from '@/components/Barchart';
 export default function Home() {
 	return (
 		<main>
-			<Barchart />
+
 		</main>
 	);
 }
