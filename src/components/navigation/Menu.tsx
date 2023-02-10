bdabbdb1cad99bef26c6e72836fb51b6442c52ff
Axel Enghamre <axel@enghamre.se>
2023-02-10 13:27:22 +0100

    simple menu animation

--- src/components/navigation/Menu.tsx
+++ src/components/navigation/Menu.tsx
@@ -1,7 +1,10 @@
 export default function Menu({menuIsVisible}:{menuIsVisible: boolean}) {
 	return (
-		<nav className={` ${menuIsVisible? '' : ''}`}>
+		<nav className={`fixed inset-0 min-h-screen bg-gradient-to-b from-white to-atlantic duration-500 transition-transform overflow-hidden p-12 ${menuIsVisible? 'w-screen -translate-x-0' : ' translate-x-full'}`}>
+			<p className=" text-mediterranean text-6xl">WATERWORKS</p>
+			<ul>
 
+			</ul>
 		</nav>
 	);
 }
