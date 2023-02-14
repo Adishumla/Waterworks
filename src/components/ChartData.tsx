8265e4ec6de2df7e79e983364cc6a576bb9775ea
Adishumla <45212964+Adishumla@users.noreply.github.com>
2023-02-14 13:53:27 +0100

    Fixed chartDisplay

--- src/components/ChartData.tsx
+++ src/components/ChartData.tsx
@@ -4,16 +4,9 @@ export default async function displayData() {
 	const url = 'http://data.goteborg.se/RiverService/v1.1/MeasureSites/4a9a0d23-8e98-4e64-81ac-3e01fed82bee?format=Json';
 	const response = await Promise.resolve(fetch(url));
 	const chartData = await response.json();
-	return (
-		<>
-			<h1>Display Data</h1>
-			{chartData.map((item: any) => (
-				<div key={item.id}>
-					<h2>{item.Code}</h2>
-					<p>{item.Description}</p>
-					<p>{item.Lat}</p>
-				</div>
-			))}
-		</> */
-	};
+	const codes = [];
+	for (const data of chartData) {
+		codes.push(data.Code);
+	}
+	return codes;
 }
--- src/components/ChartDisplay.tsx
+++ src/components/ChartDisplay.tsx
@@ -1,4 +1,5 @@
 'use client';
+import React, { useState, useEffect } from 'react';
 import Utils from 'chart.js';
 import { Bar } from 'react-chartjs-2';
 import { Line } from 'react-chartjs-2';
@@ -7,62 +8,78 @@ import ChartData from '@/components/ChartData';
 import { Chart, registerables } from 'chart.js';
 
 Chart.register(...registerables);
-
-
 async function displayData() {
-	const url = 'http://data.goteborg.se/RiverService/v1.1/MeasureSites/4a9a0d23-8e98-4e64-81ac-3e01fed82bee?format=Json';
-	const response = await Promise.resolve(fetch(url));
+	const url =
+		'https://data.goteborg.se/RiverService/v1.1/Measurements/4a9a0d23-8e98-4e64-81ac-3e01fed82bee/Agnesberg/Level/2022-01-01/2022-06-25?format=Json';
+	const response = await fetch(url);
 	const chartData = await response.json();
-	return (
-		chartData
-	);
+	const value = [];
+	console.log(chartData[0]);
+
+	for (const data of chartData) {
+		value.push(data.Value);
+	}
+	console.log(value);
+
+	const codes = [];
+	for (const data of chartData) {
+		codes.push(data.Code);
+	}
+	return { value, codes };
 }
 
-const importData = displayData();
-
-console.log(importData);
-
-//create a new chart with Bar from react-chartjs-2
-const data = {
-	labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
-	datasets: [
-		{
-			label: '# of Votes',
-			data: [12, 19, 3, 5, 2, 3],
-			backgroundColor: [
-				'rgba(255, 99, 132, 0.2)',
-				'rgba(54, 162, 235, 0.2)',
-				'rgba(255, 206, 86, 0.2)',
-				'rgba(75, 192, 192, 0.2)',
-				'rgba(153, 102, 255, 0.2)',
-				'rgba(255, 159, 64, 0.2)'
-			],
-			borderColor: [
-				'rgba(255, 99, 132, 1)',
-				'rgba(54, 162, 235, 1)',
-				'rgba(255, 206, 86, 1)',
-				'rgba(75, 192, 192, 1)',
-				'rgba(153, 102, 255, 1)',
-				'rgba(255, 159, 64, 1)'
-			],
-			borderWidth: 1
-		}
-	]
-};
-
-const options = {
-	scales: {
-		y: {
-			beginAtZero: true
-		}
-	}
-};
-
 export default function ChartDisplay() {
+	const [importData, setImportData] = useState<string[]>([]);
+	const [importLat, setImportLat] = useState<string[]>([]);
+
+	useEffect(() => {
+		async function fetchData() {
+			const codes = await displayData();
+			setImportData(codes.codes);
+			setImportLat(codes.value);
+		}
+		fetchData();
+	}, []);
+	//console.log(importData);
+	const data = {
+		labels: ['Agnessbers', 'Bergsjön', 'Björkängen', 'Björkängen', 'Björkängen', 'Björkängen'],
+		datasets: [
+			{
+				label: '# of Votes',
+				data: importLat,
+				backgroundColor: [
+					'rgba(255, 99, 132, 0.2)',
+					'rgba(54, 162, 235, 0.2)',
+					'rgba(255, 206, 86, 0.2)',
+					'rgba(75, 192, 192, 0.2)',
+					'rgba(153, 102, 255, 0.2)',
+					'rgba(255, 159, 64, 0.2)'
+				],
+				borderColor: [
+					'rgba(255, 99, 132, 1)',
+					'rgba(54, 162, 235, 1)',
+					'rgba(255, 206, 86, 1)',
+					'rgba(75, 192, 192, 1)',
+					'rgba(153, 102, 255, 1)',
+					'rgba(255, 159, 64, 1)'
+				],
+				borderWidth: 1
+			}
+		]
+	};
+
+	const options = {
+		scales: {
+			y: {
+				beginAtZero: true
+			}
+		}
+	};
+
 	return (
 		<div>
 			<p>bar-chart</p>
-			<Line data={data} options={options} />
+			<Bar data={data} options={options} />
 		</div>
 	);
 }
