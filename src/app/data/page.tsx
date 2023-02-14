d3deccd914ea56ea7806dcc08315c811d3158064
Adishumla <45212964+Adishumla@users.noreply.github.com>
2023-02-14 14:27:03 +0100

    fixed errors

--- src/app/data/page.tsx
+++ src/app/data/page.tsx
@@ -1,11 +1,11 @@
 import ChartDisplay from '@/components/ChartDisplay';
 
-
 const API_KEY = process.env.API_KEY;
 export default function Data() {
 	return (
 		<main className='bg-gradient-to-b from-white to-atlantic'>
-			<ChartDisplay apiKey={API_KEY}/>
+			{/* send apikey to chartdisplay */}
+			<ChartDisplay apiKey={API_KEY} />
 		</main>
 	);
 }
