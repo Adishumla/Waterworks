06b15076096555e054c3fbecd7ad6261006736ea
Josef Forkman <strand.vatten@outlook.com>
2023-02-21 13:48:03 +0100

    add link to GBG api heder

--- src/components/navigation/Menu.tsx
+++ src/components/navigation/Menu.tsx
@@ -21,7 +21,7 @@ export default function Menu({ menuIsVisible, onClick }: { menuIsVisible: boolea
 					</Link>
 				</li>
 				<li className='text-4xl pt-12 text-salt'>
-					<Link onClick={onClick} href='/info'>
+					<Link target={'_blank'} onClick={onClick} href='https://goteborg.se/wps/portal/start/kommun-o-politik/kommunfakta/digitalisering/oppna-data/oppna-data-soksida/oppna-data-datamangd#esc_entry=479&esc_context=6'>
 						HOW IT WORKS
 					</Link>
 				</li>
